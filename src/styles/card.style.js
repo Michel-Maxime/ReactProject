30ce9e18e51d994bf9a64fafc92972e404d753a3
Maxime MICHEL <m.michel@betclicgroup.com>
2022-03-08 17:55:11 +0100

    add heart button

---
+++ src/styles/card.style.js
@@ -0,0 +1,11 @@
+import { createUseStyles } from "react-jss";
+
+const cardStyles = createUseStyles({
+  button: {
+    border: "none",
+    background: "transparent",
+    opacity: 0.5,
+    fontSize: 16,
+  },
+});
+export default cardStyles;
